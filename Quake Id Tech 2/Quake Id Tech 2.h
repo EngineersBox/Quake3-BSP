21778b666456bb77f7f107eecb90c797e8632c57
EngineersBox <toastcraft.info@gmail.com>
2021-12-09 21:57:10 +1100

    Fixed SDL2 install, refactored to use vcpkg

--- Quake Id Tech 2/Quake Id Tech 2.h
+++
@@ -1,8 +0,0 @@
-﻿// Quake Id Tech 2.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
---
+++ src/g_main.cpp
@@ -0,0 +1,13 @@
+﻿#include "g_main.h"
+#include <SDL2/SDL.h>
+
+int main(int, char**) {
+	std::cout << "RUNNING LOAD STUFFS" << std::endl;
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "Error intializing SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	std::cout << "SDL successfully initialized" << std::endl;
+	SDL_Quit();
+	return 0;
+}
