96b09ee37ec10603a3b1a60a8f386d442122bc75
EngineersBox <toastcraft.info@gmail.com>
2021-12-09 17:39:16 +1100

    Add project files.

---
+++ Quake Id Tech 2/Quake Id Tech 2.cpp
@@ -0,0 +1,12 @@
+﻿// Quake Id Tech 2.cpp : Defines the entry point for the application.
+//
+
+#include "Quake Id Tech 2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
